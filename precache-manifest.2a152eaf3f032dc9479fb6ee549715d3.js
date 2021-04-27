21a8c970a823371965ba382248682b34c31b3454
webdevpathologist <71933275+webdevpathologist@users.noreply.github.com>
2021-04-27 11:14:36 +0530

    Git to Netlify Link Establishing

---
+++ precache-manifest.2a152eaf3f032dc9479fb6ee549715d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33f17aa6f1b96c5209867dedd91ed05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "679e242325c6108634d3",
+    "url": "/static/css/main.5e5a8f77.chunk.css"
+  },
+  {
+    "revision": "0bb78a0ff32362491edc",
+    "url": "/static/js/2.403bbdf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.403bbdf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679e242325c6108634d3",
+    "url": "/static/js/main.78fdb1c2.chunk.js"
+  },
+  {
+    "revision": "79e76fcaa52a05693c1c",
+    "url": "/static/js/runtime-main.1f6e9381.js"
+  }
+]);
